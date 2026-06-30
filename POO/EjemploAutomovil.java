5e9a2c7fcea2ac82b4576642ef295b04675841ba
JustNoel05 <fvpootisthegoat@gmail.com>
2026-06-30 12:58:37 -0600

    Entender privates, getters y setters

--- POO/EjemploAutomovil.java
+++ POO/EjemploAutomovil.java
@@ -4,18 +4,27 @@ public class EjemploAutomovil {
     public static void main(String[] args) {
         
         Automovil ford = new Automovil();
-        ford.fabricante = "ford";
-        ford.modelo = "focus";
-        ford.cilindrada = 6.9;
-        ford.color = "Azul";
+        ford.setFabricante("ford");
+        ford.setModelo("Focus");
+        ford.setCilindrada(6.9);
+        ford.setColor("Azul");
 
         Automovil mazda = new Automovil();
-        mazda.fabricante = "Mazda";
-        mazda.modelo = "BT-50";
-        mazda.cilindrada = 3.0;
-        mazda.color = "Rojo";
+        mazda.setFabricante("Huitrado");
+        mazda.setModelo("Uwuntu");
+        mazda.setCilindrada(6.9);
+        mazda.setColor("Cafe");
 
-        System.out.println(ford.detalle());
-        System.out.println(mazda.detalle());
+        System.out.println("Mazda.fabricante = " + mazda.getFabricante());
+
+        System.out.println(ford.verDetalle());
+        System.out.println(mazda.verDetalle());
+        System.out.println(ford.acelerar(300));
+        System.out.println(ford.frenar());
+
+        System.out.println(mazda.acelerarFrenar(4000));
+
+        System.out.println("Kilometros por litro: " + ford.calcularConsumo(300, .6f));
+        System.out.println("Kilometros por litro: " + ford.calcularConsumo(300, 60));
     }
 }
